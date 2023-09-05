3577f3ba40683284d7c7326041842beb635cee27
cryptomarkettrack <crypto.market.track@gmail.com>
2023-09-05 07:05:59 +0300

    try catch

--- analyzator.js
+++ analyzator.js
@@ -27,49 +27,55 @@ export const drawChart = async (assetOnExchanges) => {
 }
 
 const processPair = async (page, asset, exchange) => {
-  if (exchange === 'Kucoin') {
-    exchange = 'KuCoin';
+  let image;
+
+  try {
+      if (exchange === 'Kucoin') {
+        exchange = 'KuCoin';
+      }
+
+      if (exchange === 'Mexc') {
+        return;
+      }
+
+      if (exchange == 'Gateio') {
+        exchange = 'Gate'
+      }
+      
+      //Search and select an asset
+      await page.waitForSelector('#quickviewform-searchcoin');
+      await page.type('#quickviewform-searchcoin', asset);
+      await page.waitForSelector(`[data-value="${asset}-USDT-${exchange}"]`);
+      await page.click(`[data-value="${asset}-USDT-${exchange}"]`);
+
+      //Click 4h frame
+      await page.waitForSelector(`#${asset}USDT > td:nth-child(4)`);
+      await page.click(`#${asset}USDT > td:nth-child(4)`);
+
+      //Display patterns on the chart
+      await page.waitForSelector('#the-chart');
+      await clickInput(page, '#ma50100-input');
+      await clickInput(page, '#continuation_falling_wedge-input');
+      await clickInput(page, '#reversal_falling_wedge-input');
+      await clickInput(page, '#bullish_pennant-input');
+      await clickInput(page, '#bull_flag-input');
+      await clickInput(page, '#ascending_triangle-input');
+      await clickInput(page, '#symmetrical_triangle-input');
+      await clickInput(page, '#bearish_pennant-input');
+      await clickInput(page, '#trendline-input');
+      
+      //move the cursor away
+      await page.mouse.move(1, 1);
+
+      // Capture a screenshot and save it as a JPG file
+      await new Promise(resolve => setTimeout(resolve, 1000)); // 1 sec
+      image = await page.screenshot({ type: 'jpeg', quality: 100, omitBackground: true });
+
+      //Hide the chart and proceed to next asset if present
+      await page.click('a.hide-chart');
+  } catch (e) {
+    console.log('An exception occured during drawing analysing chart for asset ', asset, e);
   }
-
-  if (exchange === 'Mexc') {
-    return;
-  }
-
-  if (exchange == 'Gateio') {
-    exchange = 'Gate'
-  }
-  
-  //Search and select an asset
-  await page.waitForSelector('#quickviewform-searchcoin');
-  await page.type('#quickviewform-searchcoin', asset);
-  await page.waitForSelector(`[data-value="${asset}-USDT-${exchange}"]`);
-  await page.click(`[data-value="${asset}-USDT-${exchange}"]`);
-
-  //Click 4h frame
-  await page.waitForSelector(`#${asset}USDT > td:nth-child(4)`);
-  await page.click(`#${asset}USDT > td:nth-child(4)`);
-
-  //Display patterns on the chart
-  await page.waitForSelector('#the-chart');
-  await clickInput(page, '#ma50100-input');
-  await clickInput(page, '#continuation_falling_wedge-input');
-  await clickInput(page, '#reversal_falling_wedge-input');
-  await clickInput(page, '#bullish_pennant-input');
-  await clickInput(page, '#bull_flag-input');
-  await clickInput(page, '#ascending_triangle-input');
-  await clickInput(page, '#symmetrical_triangle-input');
-  await clickInput(page, '#bearish_pennant-input');
-  await clickInput(page, '#trendline-input');
-  
-  //move the cursor away
-  await page.mouse.move(1, 1);
-
-  // Capture a screenshot and save it as a JPG file
-  await new Promise(resolve => setTimeout(resolve, 1000)); // 1 sec
-  const image = await page.screenshot({ type: 'jpeg', quality: 100, omitBackground: true });
-
-  //Hide the chart and proceed to next asset if present
-  await page.click('a.hide-chart');
 
   return image;
 }
